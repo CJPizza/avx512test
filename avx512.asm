ddbd136a51ddb387cf872d711732d85d672d17b2
Carl Justine S. Alamay <carl_alamay@dlsu.edu.ph>
2025-09-16 00:45:01 +0800

    Update avx512.asm

--- avx512.asm
+++ avx512.asm
@@ -1,13 +1,9 @@
-global avx512              ; export symbol to C++
-
-section .data
-seq: db 'ACGTACGTACGTACGT', 0
-
 section .text
-avx512:
-    ; First argument in Linux x86-64 is RDI
-    mov rax, rdi            ; copy output pointer into rax (not needed, just demo)
-    mov byte [rdi], 'X'     ; store a test character into buffer
-    ret
+global _start
+_start:
+    mov eax, 60     ; sys_exit
+    xor edi, edi
+    syscall
+
 
 
