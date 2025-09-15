361c63edba543dd1a52397f6ba629ab563a47f81
Carl Justine S. Alamay <carl_alamay@dlsu.edu.ph>
2025-09-16 00:56:57 +0800

    Update avx512.asm

--- avx512.asm
+++ avx512.asm
@@ -1,6 +1,18 @@
-section .text
-global _start
-_start:
-    mov eax, 60
-    xor edi, edi
-    syscall
+option casemap:none
+public avx512
+
+.data
+seq db 'ACGTACGTACGTACGT'  ; DNA bases (ASCII)
+
+.code
+avx512 proc
+    vmovdqu8 zmm0, ZMMWORD PTR [seq]
+
+    vextracti32x4 xmm1, zmm0, 0
+
+    movdqu XMMWORD PTR [rcx], xmm1
+
+    ret
+avx512 endp
+
+end
